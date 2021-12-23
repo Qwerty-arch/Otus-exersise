01a25b3c137b49027a079930d1c62460d2456f0b
Qwerty <rosna-m@mail.ru>
2021-12-23 21:17:48 +0800

    Commit 5. add configurations

---
+++ exercise-7/src/test/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO authors (`name`)
+VALUES
+('Dan Brown'),
+('Dan Brown');
+
+INSERT INTO genres (`name`)
+VALUES
+('Detective'),
+('Roman');
+
+INSERT INTO books (`title`)
+VALUES
+('The Da Vinci Code'),
+('Angels and Demons');
